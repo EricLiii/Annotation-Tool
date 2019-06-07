408393eab800204a452de1b2b886e1438c8174e6
Li Zefeng <lzf940514@gmail.com>
2019-06-07 14:35:46 +0800

    tc

--- annotation_yolo.py
+++ annotation_yolo.py
@@ -5,25 +5,33 @@ class AnnotationYOLO():
     def __init__(self):
         pass
 
-    def read_label(self, label_path):        
-        with open(label_path, "r") as f:
-            saved_label = []
-            lines = f.readlines()
-            for i in range(len(lines)):
-                line = lines[i].split()
-                for j in range(len(line)):
-                    line[j] = float(line[j])
-                saved_label.append(line)
-        return saved_label
+    def read_label(self, label_path):      
+        if os.path.isfile(label_path):  
+            with open(label_path, "r") as f:
+                saved_label = []
+                lines = f.readlines()
+                for i in range(len(lines)):
+                    line = lines[i].split()
+                    one_list = [int(line[0])]
+                    coor_list = []
+                    for j in range(1, len(line)):                   
+                        coor_list.append(float(line[j]))
+                    one_list.append(coor_list)
+                    saved_label.append(one_list)
+            return saved_label
+        else:
+            print("There is no label file!")
 
     def write_label(self, saved_label, label_path):
         saved_rects_copy = copy.deepcopy(saved_label)
         with open(label_path, "w") as f:
             lines = []
             for item in saved_rects_copy:
-                for i in range(len(item)):
-                    item[i] = str(item[i])
-                line = " ".join(item)
+                line = []
+                line.append(str(item[0]))
+                for i in range(len(item[1])):
+                    line.append("%.5f"%item[1][i])
+                line = " ".join(line)
                 lines.append(line + "\n")
             for line_ in lines:
                 f.write(line_)
--- sss/test.py
+++
@@ -1,2 +0,0 @@
-def high(self):
-    print("sss")
